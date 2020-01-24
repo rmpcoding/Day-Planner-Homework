ca783482c7d3fc500acdb05d6c2d3ba50cf9b808
Robert Parsons <rmpcoding@gmail.com>
2020-01-23 19:15:48 -0800

    adds all of the necessary layout. Most of the css is done, except for the dynamic part of it. Javascript is needed to power the dynamic part of the assignment

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 /* 
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
-The app should display standard business hours (9 a.m. to 5 p.m.). Each time slot should represent one hour and contain the following:
-
-The time
-
-A field to hold user input
-
-A save button
-
 Clicking on the save button will store the time and user input in localStorage.
 
-Near the top of the calendar, the application should display the current day. Additionally, each hour should be color coded to reflect whether the time slot is in the past, the present, or the future. This will change depending on the time of day.
+Additionally, each hour should be color coded to reflect whether the time slot is in the past, the present, or the future. This will change depending on the time of day.
 
-You'll need to use the Moment.js library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+
 */
+const currentTime = document.createTextNode(moment().format("dddd, MMMM Do YYYY, h:mm a"))
+const targetCurrentTime = document.body.querySelector(".current-time-h2")
 
-// 
+targetCurrentTime.append(currentTime);
+
+console.log(targetCurrentTime)
+console.log(currentTime)
+
+// GET RID OF INPUT FIELDS
+  // REPLACE WITH SPAN ELEMENTS
+// Add CSS & JS to tr to change colors dynamically, depending on time of day.
+// user enters task
+  // save button ONCLICK 
+    // SETS to local storage
+    // does not lose data after refreshing page.
